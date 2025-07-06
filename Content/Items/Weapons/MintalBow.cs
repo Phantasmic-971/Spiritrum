a701ac2cb0ec39957cf9b773dd7d0d8e16a5baa1
awtism-manifesto <awpower005@gmail.com>
2025-07-06 11:36:58 -0400

    Increased damage of the Mintal Bow
    proving a point to mag about how easy it is to edit files

--- Content/Items/Weapons/MintalBow.cs
+++ Content/Items/Weapons/MintalBow.cs
@@ -38,7 +38,7 @@ namespace Spiritrum.Content.Items.Weapons
 
             // Weapon Properties
             Item.DamageType = DamageClass.Ranged; // Sets the damage type to ranged.
-            Item.damage = 38; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
+            Item.damage = 41; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
             Item.knockBack = 1.5f; // Sets the item's knockback. Note that projectiles shot by this weapon will use its and the used ammunition's knockback added together.
             Item.noMelee = true; // So the item's animation doesn't do damage.
 
